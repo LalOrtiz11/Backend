29da4757140cbe2ec36ce549e83757f0a5dd1b2d
LalOrtiz11 <eduardo_ortizm@outlook.com>
2024-01-25 22:59:12 -0600

    Actualizacion de ultimos cambios y pruebas

---
+++ backend/controllers/tareasController.js
@@ -0,0 +1,23 @@
+const getTareas = (req, res) => {
+    res.status(200).json({ mensaje: 'Hija todo va bien' })
+}
+
+const createTareas = (req, res) => {
+    res.status(201).json({ mensaje: 'Create Tareas' })
+}
+
+const updateTareas = (req, res) => {
+    res.status(200).json({ mensaje: `Modificar la tarea con id ${req.params.id}` })
+}
+
+const deleteTareas = (req, res) => {
+    res.status(200).json({ id: req.params.id })
+}
+
+
+module.exports = {
+    getTareas,
+    createTareas,
+    updateTareas,
+    deleteTareas
+}
